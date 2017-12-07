230cce82aa543d2a722699d1dd452875c1e2c67c
Hpyer <hpyer@HpyerdeMacBook-Pro.local>
2017-12-07 15:31:37 +0800

    fix bug

--- src/libs/server.js
+++ src/libs/server.js
@@ -47,7 +47,7 @@ const serve = async function () {
     if ($server_handler && typeof $server_handler == 'function') {
       let result = await $server_handler($server_message);
 
-      if (!result || result.toUpperCase() == 'SUCCESS') {
+      if (!result || (typeof result == 'string' && result.toUpperCase() == 'SUCCESS')) {
         app.sendResponse('SUCCESS');
         return
       }
