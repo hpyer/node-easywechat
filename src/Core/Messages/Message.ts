9277eecbea77a6c7dfac532403c3bae386a67fbf
Hpyer <me@hpyer.cn>
2021-01-05 20:30:10 +0800

    update: 整合message对象基础方法

--- src/Core/Messages/Message.ts
+++ src/Core/Messages/Message.ts
@@ -2,9 +2,9 @@
 
 import * as Merge from 'merge';
 import * as Xml2js from 'xml2js';
-import HasAttributesMixin from '../Mixins/HasAttributesMixin';
+import { isArray, inArray } from "../Utils";
 
-export class Message extends HasAttributesMixin
+export class Message
 {
   static TEXT: number = 2;
   static IMAGE: number = 4;
@@ -33,7 +33,6 @@ export class Message extends HasAttributesMixin
 
   constructor(attributes: object = {})
   {
-    super();
     this.setAttributes(attributes);
   }
 
@@ -105,6 +104,100 @@ export class Message extends HasAttributesMixin
     }
 
     return data;
+  }
+
+
+
+  protected attributes: object = {};
+
+  protected required: Array<string> = [];
+
+  setAttributes(attributes: object): Message
+  {
+    this.attributes = attributes;
+
+    return this;
+  }
+
+  setAttribute(name: string, value: string): Message
+  {
+    this.attributes[name] = value;
+
+    return this;
+  }
+
+  set(name: string, value: string): Message
+  {
+    this.setAttribute(name, value);
+
+    return this;
+  }
+
+  getAttribute(name: string, defaultValue: any = null): any
+  {
+    return this.attributes[name] || defaultValue;
+  }
+
+  get(name: string, defaultValue: any = null): any
+  {
+    return this.getAttribute(name, defaultValue);
+  }
+
+  has(name: string): boolean
+  {
+    for (let k in this.attributes) {
+      if (k === name) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  merge(attributes: object): Message
+  {
+    this.attributes = Merge(this.attributes, attributes);
+
+    return this;
+  }
+
+  only(keys: Array<string>): object
+  {
+    let attributes: object = {};
+    for (let k in this.attributes) {
+      keys.forEach(key => {
+        if (k === key) {
+          attributes[k] = this.attributes[k];
+        }
+      });
+    }
+
+    return attributes;
+  }
+
+  all(): object
+  {
+    this.checkRequiredAttributes();
+
+    return this.attributes;
+  }
+
+  getRequired(): Array<string>
+  {
+    return this.required && isArray(this.required) ? this.required : [];
+  }
+
+  isRequired(attribute): boolean
+  {
+    return inArray(attribute, this.getRequired(), true);
+  }
+
+  protected checkRequiredAttributes (): void
+  {
+    this.getRequired().forEach(attribute => {
+      if (attribute == null) {
+        throw new Error(`"${attribute}" cannot be empty.`);
+      }
+    });
   }
 
 };
