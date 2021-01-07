49337e85d193b39ecd3baf0ae8fb8be2a03a80ac
Hpyer <me@hpyer.cn>
2021-01-07 17:17:51 +0800

    perf: 调整总对象实现方式，优化服务类型推导

--- src/index.ts
+++ src/index.ts
@@ -15,41 +15,125 @@ import StreamResponse from './Core/Http/StreamResponse';
 import * as Messages from './Core/Messages';
 import { EasyWechatConfig } from './Core/Types';
 
-const EasyWechat = {
+/**
+ * 通用获取实例方法
+ * @param service 服务名称，可选值：'BaseService' | 'OfficialAccount' | 'MiniProgram' | 'OpenPlatform' | 'Payment'
+ * @param config 对应的配置参数
+ */
+function getInstance(service: 'OfficialAccount', config ?: EasyWechatConfig): OfficialAccount;
+function getInstance(service: 'BaseService', config ?: EasyWechatConfig): BaseService;
+function getInstance(service: 'MiniProgram', config ?: EasyWechatConfig): MiniProgram;
+function getInstance(service: 'OpenPlatform', config ?: EasyWechatConfig): OpenPlatform;
+function getInstance(service: 'Payment', config?: EasyWechatConfig): Payment;
+function getInstance(service: 'OfficialAccount' | 'BaseService' | 'MiniProgram' | 'OpenPlatform' | 'Payment', config ?: EasyWechatConfig) {
+  try {
+    let s = strStudly(service);
+    let applicationClass = this[s];
+    return new applicationClass(config);
+  }
+  catch (e) {
+    console.log(e)
+  }
+};
+
+class EasyWechat {
+  /**
+   * 实例工厂
+   */
   Factory: {
-    OfficialAccount,
-    BaseService,
-    MiniProgram,
-    OpenPlatform,
-    Payment,
+    /**
+     * 公众号类
+     */
+    OfficialAccount: typeof OfficialAccount;
+    /**
+     * 基础类
+     */
+    BaseService: typeof BaseService;
+    /**
+     * 小程序类
+     */
+    MiniProgram: typeof MiniProgram;
+    /**
+     * 开放平台类
+     */
+    OpenPlatform: typeof OpenPlatform;
+    /**
+     * 支付类
+     */
+    Payment: typeof Payment;
 
-    getInstance: function (service: string, config: EasyWechatConfig = {}): any {
-      try {
-        service = strStudly(service);
-        let applicationClass = this[service];
-        return new applicationClass(config);
-      }
-      catch (e) {
-        console.log(e)
-      }
-    },
-  },
 
-  CacheInterface,
+    /**
+     * 通用获取实例方法
+     * @param service 服务名称，可选值：'BaseService' | 'OfficialAccount' | 'MiniProgram' | 'OpenPlatform' | 'Payment'
+     * @param config 对应的配置参数
+     */
+    getInstance: typeof getInstance;
+  };
+
+  /**
+   * 自定义缓存接口
+   */
+  CacheInterface: typeof CacheInterface;
+  /**
+   * 修饰对象，用于服务端事件处理
+   */
   Decorator: {
-    FinallResult,
-    TerminateResult,
-  },
+    FinallResult: typeof FinallResult,
+    TerminateResult: typeof TerminateResult,
+  };
+  /**
+   * 网络相关对象
+   */
   Http: {
-    Request,
-    Response,
-    StreamResponse,
-  },
+    /**
+     * 请求对象
+     */
+    Request: typeof Request,
+    /**
+     * 响应对象
+     */
+    Response: typeof Response,
+    /**
+     * 响应对象（Stream）
+     */
+    StreamResponse: typeof StreamResponse,
+  };
   /**
    * @deprecated 即将作废，请使用 Messages 代替
    */
-  Message: Messages,
-  Messages,
+  Message: typeof Messages;
+  /**
+   * 消息类型
+   */
+  Messages: typeof Messages;
+
+  constructor() {
+    this.Factory = {
+      OfficialAccount: OfficialAccount,
+      BaseService: BaseService,
+      MiniProgram: MiniProgram,
+      OpenPlatform: OpenPlatform,
+      Payment: Payment,
+      getInstance: getInstance,
+    };
+
+    this.CacheInterface = CacheInterface;
+
+    this.Decorator = {
+      FinallResult: FinallResult,
+      TerminateResult: TerminateResult,
+    }
+    this.Http = {
+      Request: Request,
+      Response: Response,
+      StreamResponse: StreamResponse,
+    }
+
+    this.Message = Messages;
+    this.Messages = Messages;
+  }
+
 };
 
-export = EasyWechat;
+export = new EasyWechat;
