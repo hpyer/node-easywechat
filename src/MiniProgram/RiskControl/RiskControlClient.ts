ab53cb33e1a9a54c0ebe81d7f21a026ae7cf552d
Hpyer <me@hpyer.cn>
2021-07-02 00:55:18 +0800

    fix: 修复小程序风控接口请求异常的问题

--- src/MiniProgram/RiskControl/RiskControlClient.ts
+++ src/MiniProgram/RiskControl/RiskControlClient.ts
@@ -5,7 +5,7 @@ import BaseClient from '../../Core/BaseClient';
 export default class RiskControlClient extends BaseClient {
 
   getUserRiskRank(params: object): Promise<any> {
-    return this.httpPost('wxa/getuserriskrank', params);
+    return this.httpPostJson('wxa/getuserriskrank', params);
   }
 
 }
