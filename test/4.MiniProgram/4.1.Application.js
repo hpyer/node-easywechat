e70bbf8803ee33b26e58abca1edfab5b7e2f17ce
Hpyer <me@hpyer.cn>
2022-01-05 23:05:58 +0800

    test: 新增小程序模块测试用例

---
+++ test/4.MiniProgram/4.1.Application.js
@@ -0,0 +1,26 @@
+
+const BaseClientTest = require('../BaseClientTest');
+
+class TestUnit extends BaseClientTest {
+
+  test() {
+
+    [
+      'access_token', 'auth', 'encryptor', 'server', 'data_cube', 'app_code',
+      'template_message', 'customer_service', 'uniform_message', 'activity_message',
+      'open_data', 'plugin', 'plugin_dev', 'express', 'nearby_poi', 'ocr', 'soter',
+      'subscribe_message', 'realtime_log', 'search', 'media', 'content_security',
+      'url_scheme', 'risk_control', 'live', 'broadcast', 'union'
+    ].map(module => {
+      it(`Should has module ${module}`, () => {
+        this.assert.strictEqual(this.app[module] && typeof this.app[module] == 'object', true);
+      });
+    })
+
+  }
+}
+
+new TestUnit('MiniProgram', 'constuctor', {
+  app_id: 'abc@123',
+  secret: '123456',
+});
---
+++ test/4.MiniProgram/4.2.Server.js
@@ -0,0 +1,74 @@
+
+const BaseClientTest = require('../BaseClientTest');
+const EasyWechat = require('../../dist');
+
+class TestUnit extends BaseClientTest {
+
+  test() {
+
+    it(`Should response echostr`, async () => {
+
+      let req = {};
+      req.url = 'https://www.example.com/wx/server?echostr=hello';
+      req.method = 'post';
+      req.headers = {
+        'content-length': 0,
+        'content-type': 'text/xml',
+        'client-ip': '123.123.123.123',
+      };
+
+      let request = new EasyWechat.Http.Request(req, {});
+      this.app.rebind('request', request);
+
+      let response = await this.app.server.serve();
+      this.assert.strictEqual(response.content.toString(), 'hello');
+    });
+
+
+
+    let xml = `<xml>
+<foo>bar</foo>
+<sign>834A25C9A5B48305AB997C9A7E101530</sign>
+</xml>`;
+    let req = {};
+    req.url = 'https://www.example.com/wx/server?foo=bar';
+    req.method = 'post';
+    req.headers = {
+      'content-length': xml.length,
+      'content-type': 'text/xml',
+      'client-ip': '123.123.123.123',
+    };
+
+    let request = new EasyWechat.Http.Request(req, xml);
+
+    let response = {
+      content: '',
+    };
+
+    it(`Should the same message`, async () => {
+      this.app.rebind('request', request);
+
+      this.app.server.push(async function (message) {
+        this.assert.deepStrictEqual(message, {
+          foo: 'bar',
+          sign: '834A25C9A5B48305AB997C9A7E101530',
+        });
+
+        return true;
+      });
+
+      response = await this.app.server.serve();
+
+    });
+
+    it(`Should response SUCCESS`, async () => {
+      this.assert.strictEqual(response.content.toString(), 'success');
+    });
+
+  }
+}
+
+new TestUnit('MiniProgram', 'Server', {
+  app_id: 'abc@123',
+  secret: '123456',
+});
