969674df19853010282f1c8e8fddbdf09ce0fe39
Hpyer <me@hpyer.cn>
2022-01-07 00:05:39 +0800

    build

--- dist/Work/ExternalContact/Client.d.ts
+++ dist/Work/ExternalContact/Client.d.ts
@@ -163,4 +163,49 @@ export default class ExternalContactClient extends BaseClient {
      * @returns
      */
     markTags(data: object): Promise<any>;
+    /**
+     * 外部联系人unionid转换
+     * @see https://work.weixin.qq.com/api/doc/90001/90143/93274
+     * @param unionid 微信客户的unionid
+     * @param openid 微信客户的openid
+     * @returns
+     */
+    unionidToExternalUserid(unionid?: string, openid?: string): Promise<any>;
+    /**
+     * 代开发应用external_userid转换
+     * @see https://work.weixin.qq.com/api/doc/90001/90143/95195
+     * @param unionid 微信客户的unionid
+     * @returns
+     */
+    toServiceExternalUserid(externalUserid: string): Promise<any>;
+    /**
+     * 转换external_userid
+     * @see https://open.work.weixin.qq.com/api/doc/90001/90143/95327#%E8%BD%AC%E6%8D%A2external_userid
+     * @param externalUserIds
+     * @returns
+     */
+    getNewExternalUserid(externalUserIds: string[]): Promise<any>;
+    /**
+     * 设置迁移完成
+     * @see https://open.work.weixin.qq.com/api/doc/90001/90143/95327#%E8%AE%BE%E7%BD%AE%E8%BF%81%E7%A7%BB%E5%AE%8C%E6%88%90
+     * @param corpid
+     * @returns
+     */
+    finishExternalUseridMigration(corpid: string): Promise<any>;
+    /**
+     * unionid查询external_userid
+     * @see https://open.work.weixin.qq.com/api/doc/90001/90143/95327#4.2%20unionid%E6%9F%A5%E8%AF%A2external_userid
+     * @param unionid
+     * @param openid
+     * @param corpid
+     * @returns
+     */
+    unionidToexternalUserid3rd(unionid: string, openid: string, corpid?: string): Promise<any>;
+    /**
+     * 客户群opengid转换
+     * @see https://work.weixin.qq.com/api/doc/90000/90135/94822
+     * @param opengid
+     * @returns
+     */
+    opengidToChatid(opengid: string): Promise<any>;
 }
