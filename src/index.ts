46ed8d6dd7fa89e6df1f05714f3b0c69314468ea
Hpyer <me@hpyer.cn>
2022-06-16 17:39:47 +0800

    feat: 增加统一的对象调用入口

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import OfficialAccount from './OfficialAccount/Application';
+import MiniApp from './MiniApp/Application';
+import CacheInterface from './Core/Contracts/CacheInterface';
+import ServerRequest from './Core/Http/ServerRequest';
+
+export {
+  OfficialAccount,
+  MiniApp,
+
+  CacheInterface,
+  ServerRequest,
+}
