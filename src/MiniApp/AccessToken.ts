dd41990cc114b5ae850e1a4a4874485cfb5fddd3
Hpyer <me@hpyer.cn>
2022-06-16 18:27:22 +0800

    fix: 修复微信小程序access_token键名前缀与公众号相同的问题

--- src/MiniApp/AccessToken.ts
+++ src/MiniApp/AccessToken.ts
@@ -5,6 +5,17 @@ import BaseAccessToken from "../OfficialAccount/AccessToken";
 class AccessToken extends BaseAccessToken
 {
 
+  /**
+   * 获取access_token的缓存名称
+   * @returns
+   */
+  getKey(): string {
+    if (!this.key) {
+      this.key = `mini_app.access_token.${this.appId}`;
+    }
+    return this.key;
+  }
+
 }
 
 export = AccessToken;
