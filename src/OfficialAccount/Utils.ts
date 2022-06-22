92bd144f677e651c94bed57dcbc267fccb791a88
Hpyer <me@hpyer.cn>
2022-06-22 23:29:03 +0800

    doc: 新增构建jssdk配置说明

--- src/OfficialAccount/Utils.ts
+++ src/OfficialAccount/Utils.ts
@@ -7,6 +7,14 @@ class Utils
 {
   constructor(protected app: Application) {}
 
+  /**
+   * 构建jssdk配置
+   * @param url 完整URL地址
+   * @param jsApiList api列表，默认：[]。可用列表：https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html#63
+   * @param openTagList 开放标签列表，默认：[]。可用列表：https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/Wechat_Open_Tag.html#附录-所有开放标签列表
+   * @param debug 是否开启调试模式，默认：false
+   * @returns
+   */
   async buildJsSdkConfig(url: string, jsApiList: string[] = [], openTagList: string[] = [], debug: boolean = false): Promise<Record<string, any>>
   {
     return merge({
