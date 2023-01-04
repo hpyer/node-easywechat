b88c9dde9f5dce64d41bcbf8575042e31c0bbaef
Hpyer <me@hpyer.cn>
2023-01-04 09:57:19 +0800

    feat: 企业微信新增更新任务卡片消息、更新模版卡片消息两个接口

--- src/Work/Message/MessageClient.ts
+++ src/Work/Message/MessageClient.ts
@@ -17,4 +17,26 @@ export default class MessageClient extends BaseClient
     return this.httpPostJson('cgi-bin/message/send', data);
   }
 
+  /**
+   * 更新任务卡片消息
+   * @see https://open.work.weixin.qq.com/api/doc/90000/90135/91579
+   * @param data
+   * @returns
+   */
+  updateTaskcard(data: object): Promise<any>
+  {
+    return this.httpPostJson('cgi-bin/message/update_taskcard', data);
+  }
+
+  /**
+   * 更新模版卡片消息
+   * @see https://developer.work.weixin.qq.com/document/path/94888
+   * @param data
+   * @returns
+   */
+  updateTemplateCard(data: object): Promise<any>
+  {
+    return this.httpPostJson('cgi-bin/message/update_template_card', data);
+  }
+
 }
